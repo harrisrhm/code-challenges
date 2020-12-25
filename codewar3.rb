fffb168b973a707c3004da803cb5d27cf1b18f18
Harris Rahim <harris.rhm@gmail.com>
2020-12-25 12:03:48 +1100

    Math.Sqrt(2)

--- codewar3.rb
+++ codewar3.rb
@@ -5,11 +5,9 @@
 # end
 
 def comp(array1)
-  sum = 0
-  array1.each do |num|
-    sum = num ** 2
-  end
-  puts sum
+  arr = []
+  array1.each { |num| arr << num ** 2 }
+  p arr
 end
 
 comp([121, 144, 19, 161, 19, 144, 19, 11])
