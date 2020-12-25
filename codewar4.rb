d82f25c238fd172f22c8a9379646a55cd936da4e
Harris Rahim <harris.rhm@gmail.com>
2020-12-25 15:14:25 +1100

    Refactored code

--- codewar4.rb
+++ codewar4.rb
@@ -5,25 +5,15 @@
 # Only lower case letters will be used (a-z). No punctuation or digits will be included.
 # Performance needs to be considered
 
+#Refactored solution
 def scramble(s1, s2)
-  # p s1.chars.include?(s2.chars)
-  str1 = s1.chars
-  str2 = s2.chars
-
-  str3 = str1.map { |letter| str2.include?(letter) }
-  p str3
-
-  # p s2.chars == s1.chars ? true : false
-
-  # s1.chars.select { |l| p s2.chars.include?(l) }
-  # s1.sort.include?(s2.sort)
-  # s1.downcase.split("")
-  # str2 = s2.downcase.split("")
-  # str1.each { |letter| p letter.include?(str2) }
-  # s1.downcase.each do |l|
-  #   str2 = s2.downcase.split(",")
-  #   p l.include?(str2)
-  # end
+  # p s1.chars.include?(s2)
+  p s2.chars.uniq.all?{ |letter| s2.count(letter) <= s1.count(letter) }
 end
 
-scramble('rkqodlw', 'world')
+scramble('katas','steak')
+
+# First solution but failed a test spec.
+# def scramble(s1)
+#   s1.chars.map { |letter| s2.chars.include?(letter) }.include?(false)
+# end
