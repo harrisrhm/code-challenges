8086d0bd180f574c30fe15b487337b6bebd88bb3
Harris Rahim <harris.rhm@gmail.com>
2020-12-28 16:40:48 +1100

    Working solution

--- codewar7.rb
+++ codewar7.rb
@@ -10,24 +10,42 @@
 # rgb(0,0,0) # returns 000000
 # rgb(148, 0, 211) # returns 9400D3
 
-def rgb(r, g, b)
- p [r, g, b].map { |x| x >= 0 && x <= 255 ? '%02X'%x.abs : x + 000 }.join
-end
-
+# Not working
 # def rgb(r, g, b)
-#  p sprintf("%02X", r, g, b)
+#  p [r, g, b].map { |x| x >= 0 && x <= 255 ? '%02X'%x.abs : x + 000 }.join
 # end
 
-def rgb(r, g, b)
-  p [r, g, b].map { |x| x.to_s(16) }.join("").upcase
+# # def rgb(r, g, b)
+# #  p sprintf("%02X", r, g, b)
+# # end
+
+# def rgb(r, g, b)
+#   p [r, g, b].map { |x| x.to_s(16) }.join("").upcase
+# end
+
+# def rgb(r, g, b)
+#   str = ""
+#   p [r, g, b].map { |x| str << x.to_s(16).rjust(2, '0') }
+# end
+
+# def rgb(r, g, b)
+#   str = ""
+#   p [r, g, b].map { |x| str << x.to_s(16).rjust(2, '0') }
+# end
+
+# def rgb(r, g, b)
+#   p [r, g, b].map { |x| x >= 0 && x <= 255 ? x.to_s(16) : x = 000 }.join("").upcase
+# end
+
+Working solutions
+def rgb(*args)
+  format('%.2X%.2X%.2X', *(args.map { |i| [[i, 255].min, 0].max }))
 end
 
-def rgb(r, g, b)
-  str = ""
-  p [r, g, b].map { |x| str << x.to_s(16) }
-end
-
-
-def rgb(r, g, b)
-  p [r, g, b].map { |x| x >= 0 && x <= 255 ? x.to_s(16) : x = 000 }.join("").upcase
+def rgb(*args)
+  args.map do |x|
+    x = 255 if x > 255
+    x = 0 if x < 0
+    x.to_s(16).upcase.rjust(2, '0')
+  end.join
 end
