0375bd4fdc9caeaae6161b99f93138a3d17df9e7
Harris Rahim <harris.rhm@gmail.com>
2021-01-06 19:52:08 +1100

    Second commit

--- leetCode1.rb
+++ leetCode1.rb
@@ -1,3 +1,11 @@
 # Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
 # Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+
+def single_number(nums)
+  p nums.each_with_object(Hash.new(0)) { |number, value| value[number] += 1 }.find
+  # final = hash.values.all? { |value| value == 9 }
+  # final == true ? 'Finished!' : 'Try again!'
+end
+
+single_number([4,1,2,1,2])
