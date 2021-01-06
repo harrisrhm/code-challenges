9a8e68fde2cf65a330a324c31e9b951d0cc110af
Harris Rahim <harris.rhm@gmail.com>
2021-01-06 21:36:32 +1100

    Working solution

--- leetCode1.rb
+++ leetCode1.rb
@@ -2,10 +2,16 @@
 
 # Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
 
+# working solution
 def single_number(nums)
-  p nums.each_with_object(Hash.new(0)) { |number, value| value[number] += 1 }.find
-  # final = hash.values.all? { |value| value == 9 }
-  # final == true ? 'Finished!' : 'Try again!'
+  hash = {}
+  nums.each { |num| hash.key?(num) ? hash.delete(num) : hash[num] = 1  }
+  hash.key(1)
 end
 
 single_number([4,1,2,1,2])
+
+# def single_number(nums)
+# # p number = nums.each_with_object(Hash.new(0)) { |number, value| value[number] += 1 }
+# # p number[1]
+# end
