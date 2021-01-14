bceefd038266aebe718a047c7a98c770cd7f330c
Harris Rahim <harris.rhm@gmail.com>
2021-01-14 20:26:28 +1100

    Third commit

--- leetCode3.rb
+++ leetCode3.rb
@@ -1,14 +1,10 @@
 # Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
 def max_sub_array(nums)
-  start = 0
-  start_after_zero = 0
-  arr = []
-  nums.inject do |sum, n|
-    if sum + v = start
-      sum + v > 0
-    end
+  nums.inject([0, -1]) do |sum, n|
+    p arr = [sum[0] + n, sum[1] + 1]
   end
 end
+
 
 max_sub_array([-2,1,-3,4,-1,2,1,-5,4])
