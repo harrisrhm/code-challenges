b66d0881c53403abf80568b2cfe5a4810245474f
Harris Rahim <harris.rhm@gmail.com>
2021-01-25 21:03:18 +1100

    Third commit

--- leetCode4.rb
+++ leetCode4.rb
@@ -8,18 +8,37 @@
 # You must do this in-place without making a copy of the array.
 # Minimize the total number of operations.
 
+# def move_zeroes(nums)
+#     arr_one = []
+#     arr_two = []
+#     arr_three = [arr_two, arr_one]
+#     nums.each do |n|
+#       if n == 0
+#         arr_one << n
+#       else
+#         arr_two << n
+#       end
+#     end
+#     arr_three.flatten
+# end
+
+
 def move_zeroes(nums)
-    arr_one = []
-    arr_two = []
-    arr_three = [arr_two, arr_one]
-    nums.each do |n|
-      if n == 0
-        arr_one << n
-      else
-        arr_two << n
+  len = nums.length
+  count=0
+  for i in 0...len
+      if a[i]!=0
+          a[count]=a[i]
+          count+=1
       end
-    end
-    p arr_three.flatten
+  end
+# # Loop from count to len-1 and fill it with zeros 
+#   while count<len
+#       a[count]=0
+#       count+=1
+#   end
+#   return a
 end
+
 
 move_zeroes([0,1,0,3,12])
